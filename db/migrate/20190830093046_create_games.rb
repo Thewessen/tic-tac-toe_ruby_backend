4feeb2f5c1456cd04b8ecf7ae8906d32cfc35c56
Samuel Thewessen <sthewessen@gmail.com>
2019-08-31 09:40:22 +0200

    New default value boardstate, filling empty cells with ' '.

--- db/migrate/20190830093046_create_games.rb
+++ db/migrate/20190830093046_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-      t.string :boardstate, :default => ",,,,,,,,"
+      t.string :boardstate, :default => " , , , , , , , , "
 
       t.timestamps
     end
